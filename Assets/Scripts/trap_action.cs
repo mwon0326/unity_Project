447121209de95a06e0844d01410702c35bb859e4
Min Hye Won <mwon0326@gmail.com>
2020-02-02 23:13:35 +0900

    피 파티클 / 독 스프링클러 함정 / 송곳 함정

---
+++ Assets/Scripts/trap_action.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap_action : MonoBehaviour
+{
+    public ParticleSystem trap_particle; // 재생할 함정 파티클
+    public float trap_playTime; // 함정이 발동되는 시간
+    public float trap_stopTime; // 함정이 멈춰있는 시간
+
+    private Collider trap_collider;
+    private bool is_trap = false;
+    private float trap_time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trap_collider = GetComponent<Collider>();
+        StartCoroutine(TrapEffect());
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (is_trap)
+        {
+            trap_time += Time.deltaTime;
+            
+            if (trap_time >= 2f)
+            {
+                Debug.Log("Dead");
+                trap_time = 0;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (is_trap)
+            trap_time = 0;
+    }
+
+    private IEnumerator TrapEffect()
+    {
+        while (true)
+        {
+            is_trap = true;
+            trap_particle.Play();
+            yield return new WaitForSeconds(trap_playTime);
+
+            is_trap = false;
+            trap_particle.Stop();
+            trap_particle.Clear();
+            yield return new WaitForSeconds(trap_stopTime);
+        }
+    }
+}
---
+++ Assets/Scripts/trap_niddle_action.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap_niddle_action : MonoBehaviour
+{
+    public float max_height; // 최고 도달점
+    public float min_height; // 최저 도달점
+    public float velocity; // 움직이는 속도
+    public float mass;
+    public GameObject niddle;
+    
+    private bool isUP = true; // 올라가고 있는지 내려가고 있는지
+    private bool isWait = false;
+    private Collider niddle_collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        niddle_collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (niddle.transform.position.y < max_height && isUP)
+        {
+            float acc = (9.8f * mass) / mass;
+            float vel = velocity + acc * Time.deltaTime;
+            velocity = vel;
+
+            niddle.transform.Translate(Vector3.up * vel);
+            
+            if (niddle.transform.position.y >= max_height)
+            {
+                isUP = false;
+                velocity = 5f;
+            }
+        }
+        else if (niddle.transform.position.y > min_height && !isUP)
+        {
+            niddle.transform.Translate(Vector3.down * velocity);
+
+            if (niddle.transform.position.y <= min_height)
+            {
+                velocity = 5f;
+                if (!isWait)
+                    StartCoroutine(WaitTime());
+            }
+        }
+    }
+
+    IEnumerator WaitTime()
+    {
+        isWait = true;
+        yield return new WaitForSeconds(5f);
+        isUP = true;
+        isWait = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isWait)
+            Debug.Log("Dead");
+    }
+}
+
